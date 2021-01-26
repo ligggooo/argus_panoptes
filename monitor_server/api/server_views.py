faa6b8023bbca6766b775a19e8366a9d4f7dc222
ligongwei <gongweil@heading.loc>
2021-01-26 11:25:45 +0800

    added machines

--- monitor_server/api/server_views.py
+++ monitor_server/api/server_views.py
@@ -7,18 +7,28 @@ api_group1 = Blueprint("api_g1",__name__)
 
 
 @api_group1.route('/')
-def hello_world():
+def overview():
     # return 'Hello World!'
     print(url_for("api_g1.get_data", x=123,_external=True))
-    sps = SoftPackage.query.all()
-    for sp in sps:
-        print(sp)
-    return render_template("index.html",soft_packages=sps, overview_class="active")
+    return render_template("index.html",overview_class="active", pageheadershow=True, show_boards=True)
 
 @api_group1.route('/machines', methods=['GET'])
 def get_machines():
     print(request.method)
-    return render_template("index.html", machines_class="active")
+    return render_template("index.html", machines_class="active", show_boards=True)
+
+@api_group1.route('/containers', methods=['GET'])
+def get_containers():
+    print(request.method)
+    return render_template("index.html", containers_class="active")
+
+@api_group1.route('/products', methods=['GET'])
+def get_products():
+    print(request.method)
+    sps = SoftPackage.query.all()
+    for sp in sps:
+        print(sp)
+    return render_template("index.html", products_class="active",soft_packages=sps)
 
 
 @api_group1.route('/data/<x>', methods=['GET'])
---
+++ monitor_server/models/model_003_machines.py
@@ -0,0 +1,45 @@
+from sqlalchemy import UniqueConstraint, PrimaryKeyConstraint
+from monitor_server import app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
+class Machines(db.Model):
+    __tablename__ = "machines"
+    __table_args__ = (
+    )
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ip_addr = db.Column(db.String(32), nullable=False, unique=True)
+    host_name = db.Column(db.String(32), default="N/A", unique=False)
+    cpu_cores = db.Column(db.Integer, nullable=False, unique=False)
+    free_mem_in_MB = db.Column(db.Integer, nullable=False, unique=False)
+    deploy_point_1 = db.Column(db.String(256), nullable=False, unique=False)
+    free_storage_in_GB_1 = db.Column(db.Integer, nullable=False, unique=False)
+    deploy_point_2 = db.Column(db.String(256), nullable=True, unique=False)
+    free_storage_in_GB_2 = db.Column(db.Integer, nullable=True, unique=False)
+    reserve_1 = db.Column(db.String(256), nullable=True, unique=False)
+    reserve_2 = db.Column(db.String(256), nullable=True, unique=False)
+
+    def __repr__(self):
+        return "<%s %s>" % (self.ip_addr, self.host_name)
+
+    @property
+    def full_name(self):
+        return "%s[ %s ]" % (self.ip_addr, self.host_name)
+
+
+if __name__ == "__main__":
+
+    for i in range(1):
+        db.create_all()
+        new_obj = Machines(ip_addr="10.130.160.114",
+                           host_name="hc-app-1", cpu_cores=16, free_mem_in_MB=304, deploy_point_1="/mnt/", free_storage_in_GB_1=1700
+                           )
+        if Machines.query.filter_by(ip_addr=new_obj.ip_addr).limit(1).all():
+            print(new_obj, "exists")
+            continue
+        sess = db.session()
+        sess.add(new_obj)
+        sess.commit()
+        print(dir(new_obj))
+    print(Machines.query.all())
