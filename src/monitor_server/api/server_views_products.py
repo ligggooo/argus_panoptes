14619fe94ae8050e8dacd3f379bd31c9ad330e4f
ligongwei <gongweil@heading.loc>
2021-02-05 15:58:12 +0800

    fixed bug: readme.md -> README.md case mismatch is not allowed in unix.

--- src/monitor_server/api/server_views_products.py
+++ src/monitor_server/api/server_views_products.py
@@ -16,6 +16,7 @@ def overview():
     # return 'Hello World!'
     # print(url_for("api_g1.get_data", x=123,_external=True))
     print(request.method)
+    os.path.join(_proj_root, "README.md")
     index_readme = open(os.path.join(_proj_root, "readme.md"),encoding="utf-8").read().replace("\n","<br>").replace(" ","&nbsp")
     return render_template("index.html",overview_class="active", pageheadershow=True, show_boards=True,index_readme=index_readme)
 
